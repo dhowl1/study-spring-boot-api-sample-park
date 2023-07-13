d853eef397d6bd4d20eaa8aac1a4120fd6b4c1ad
Jangho <p.jangho1029@gmail.com>
2023-07-12 23:51:57 -0700

    initial commit

---
+++ src/main/java/com/example/demo/SampleProject230713Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProject230713Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProject230713Application.class, args);
+	}
+
+}
