0b9603939b7db9aa88acf3b36ad6a1a1f811cd82
dhowl1 <139436754+dhowl1@users.noreply.github.com>
2023-09-09 20:06:40 -0400

    Update V1.0.1__add_tb_member.sql

--- src/main/resources/db/migration/V1.0.1__add_tb_member.sql
+++ src/main/resources/db/migration/V1.0.1__add_tb_member.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS member(
     id                  BIGINT              AUTO_INCREMENT     PRIMARY KEY,
     username            VARCHAR(255)        NOT NULL                                                            COMMENT 'Account',
     password            VARCHAR(255)                                                                            COMMENT 'Password',
-    nickname            VARCHAR(255), -- (int) 사이즈
+    nickname            VARCHAR(255), 
     email               VARCHAR(255),
 
 
     -- Account Status
-    status            VARCHAR(255)                            DEFAULT 'PENDING'                               COMMENT 'Account status. PENDING(가입 대기) | ACTIVE | SUSPENDED | PROTECTED | SLEEP | REMOVED,
+    status            VARCHAR(255)                            DEFAULT 'PENDING'                               COMMENT 'Account status. PENDING | ACTIVE | SUSPENDED | PROTECTED | SLEEP | REMOVED,
 
     CONSTRAINT uq_account_username          UNIQUE  (username)
 );
