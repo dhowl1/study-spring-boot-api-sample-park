337436214a8c167145a091577c8ecffc66ccca7f
dhowl1 <139436754+dhowl1@users.noreply.github.com>
2023-09-09 20:03:58 -0400

    Update AuthenticationController.java

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -14,9 +14,9 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 public final class AuthenticationController {
-    // Get, Post, Put, Delete, Patch -- method get/post/.... <from action "" method ""> XHR(AJAX)..(API)
+    
 
-    private final AuthenticationService authenticationService; //<- bean 주입, 선언은 인터페이스, 주입되는 건 클레스의 빈
+    private final AuthenticationService authenticationService; 
     @PostMapping("/sign-up")
     public SignUpResponseDto signUp(@RequestBody @Valid SignUpRequestDto body) {
         return authenticationService.signUp(body, MemberStatus.ACTIVE);
